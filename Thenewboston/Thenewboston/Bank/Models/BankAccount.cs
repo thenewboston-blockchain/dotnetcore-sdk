a0a49e0bc13008a5bea7ab0fe6a8d5e0161bfd10
Mircea Teodor Oprea <mirceateodor.oprea@gmail.com>
2020-10-07 11:34:15 +0300

    Create project and bank account class

---
+++ Thenewboston/Thenewboston/Bank/Models/BankAccount.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Thenewboston.Bank.Models
+{
+    public class BankAccount
+    {
+        public string Id { get; set; }
+
+        public string AccountNumber { get; set; }
+
+        public float Trust { get; set; }
+
+        public DateTime Created { get; set; }
+
+        public DateTime Modified { get; set; }
+    }
+}
