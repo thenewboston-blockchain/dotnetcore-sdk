0c12160a747c0c5f42c245fec3a02c87f88cade9
Mircea Teodor Oprea <mirceateodor.oprea@gmail.com>
2020-10-13 19:50:55 +0300

    Add test project

---
+++ src/Thenewboston.Tests/Test.cs
@@ -0,0 +1,20 @@
+using System;
+using Xunit;
+
+namespace Thenewboston.Tests
+{
+    public class Test
+    {
+        /// <summary>
+        /// Just a simple test to prove the CI pipeline works.
+        /// </summary>
+        [Fact]
+        public void JustATest()
+        {
+            int I = 2;
+            int you = 3;
+
+            Assert.True(I < 3 * you);
+        }
+    }
+}
