d390f3fc55feb25f3e9d3309113eed9876e01931
Mircea Teodor Oprea <mirceateodor.oprea@gmail.com>
2020-10-18 23:19:02 +0300

    Create GetAccounts method

---
+++ src/Thenewboston/Accounts/BankApi/BankApiClient.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Thenewboston.Accounts.BankApi.Models;
+
+namespace Thenewboston.Accounts.BankApi
+{
+    public class BankApiClient
+    {
+        private HttpClient _client;
+        private bool _mock;
+
+        public BankApiClient(bool mock = false)
+        {
+            _mock = mock;
+            _client = new HttpClient();
+            //TODO: set the client's base address
+        }
+
+        public async Task<IEnumerable<AccountResponseModel>> GetAccounts()
+        {
+            var response = await _client.GetAsync("/accounts");
+            var stringResult = await response.Content.ReadAsStringAsync();
+
+            if (!response.IsSuccessStatusCode)
+            {
+                //TODO: create specific exception
+                throw new Exception();
+            }
+
+            var result = JsonConvert.DeserializeObject<IEnumerable<AccountResponseModel>>(stringResult);
+
+            return result;
+        }
+    }
+}
---
+++ src/Thenewboston/Accounts/BankApi/Models/AccountResponseModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Thenewboston.Accounts.BankApi.Models
+{
+    public class AccountResponseModel
+    {
+        public string Id { get; set; }
+
+        public string AccountNumber { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        public DateTime ModifiedDate { get; set; }
+
+        public double Trust { get; set; }
+    }
+}
