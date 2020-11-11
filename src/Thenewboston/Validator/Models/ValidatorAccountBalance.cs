9f45e81abd4aa50d7b509f1eb91abf799f27fe80
joshman1019 <36560776+joshman1019@users.noreply.github.com>
2020-11-11 02:11:59 -0500

    Migrated ValidatorAccountBalance and ValidatorAccountBalanceLock to their own files to help with maintainability of project.

---
+++ src/Thenewboston/Validator/Models/ValidatorAccountBalance.cs
@@ -0,0 +1,7 @@
+﻿namespace Thenewboston.Validator.Models
+{
+    public class ValidatorAccountBalance
+    {
+        public string Balance { get; set; }
+    }
+}
---
+++ src/Thenewboston/Validator/Models/ValidatorAccountBalanceLock.cs
@@ -0,0 +1,7 @@
+﻿namespace Thenewboston.Validator.Models
+{
+    public class ValidatorAccountBalanceLock
+    {
+        public string BalanceLock { get; set; }
+    }
+}
