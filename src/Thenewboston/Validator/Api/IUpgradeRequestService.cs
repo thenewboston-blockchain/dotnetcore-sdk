2a728534b6c90440fdd6164d5a40b764a2081024
joshman1019 <36560776+joshman1019@users.noreply.github.com>
2020-11-27 16:16:12 -0500

    Created Upgrade Request Service, associated models, and tests. Fixed an inefficiency in UpgradeNoticeService.

---
+++ src/Thenewboston/Validator/Api/IUpgradeRequestService.cs
@@ -0,0 +1,11 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Thenewboston.Validator.Models;
+
+namespace Thenewboston.Validator.Api
+{
+    public interface IUpgradeRequestService
+    {
+        public Task<HttpResponseMessage> PostUpgradeRequestAsync(UpgradeRequest upgradeRequest);
+    }
+}
---
+++ src/Thenewboston/Validator/Models/UpgradeRequest.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace Thenewboston.Validator.Models
+{
+    public class UpgradeRequest
+    {
+        [JsonProperty(PropertyName ="")]
+        public UpgradeRequestMessage Message { get; set; }
+
+        [JsonProperty(PropertyName ="node_identifier")]
+        public string NodeIdentifier { get; set; }
+
+        [JsonProperty(PropertyName ="signature")]
+        public string Signature { get; set; }
+    }
+}
