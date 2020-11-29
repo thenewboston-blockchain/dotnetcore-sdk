4377e78fd7efdaf2d45d974f8b1bfb047797ec2d
joshman1019 <36560776+joshman1019@users.noreply.github.com>
2020-11-28 20:22:11 -0500

    Create Primary Validator Updated Models

---
+++ src/Thenewboston/Validator/Models/PrimaryValidatorUpdatedResponse.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace Thenewboston.Validator.Models
+{
+    public class PrimaryValidatorUpdatedResponse
+    {
+        [JsonProperty(PropertyName ="message")]
+        public PrimaryValidatorUpdatedMessage Message { get; set; }
+
+        [JsonProperty(PropertyName ="property_name")]
+        public string NodeIdentifier { get; set; }
+
+        [JsonProperty(PropertyName ="signature")]
+        public string Signature { get; set; }
+    }
+}
