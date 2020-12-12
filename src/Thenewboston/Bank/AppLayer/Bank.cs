037d7085bcdffba8120e53c727c075a33d5ade02
Mircea Teodor Oprea <mirceateodor.oprea@gmail.com>
2020-12-12 16:05:34 +0200

    Add small example

---
+++ src/Thenewboston/Bank/AppLayer/Bank.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Thenewboston.Bank.Api;
+using Thenewboston.Bank.Models;
+using Thenewboston.Common.Http;
+
+namespace Thenewboston.Bank.AppLayer
+{
+    public class Bank
+    {
+        private readonly AccountsService _accountsService;
+
+        public Bank(string ip, int port)
+        {
+            var requestSender = new SimpleHttpRequestSender($"{ip}:{port}");
+            _accountsService = new AccountsService(requestSender);
+        }
+
+        public async Task<IEnumerable<BankAccount>> GetAccounts()
+        {
+            var result = await _accountsService.GetAccountsAsync();
+            return result;
+        }
+
+    }
+}
